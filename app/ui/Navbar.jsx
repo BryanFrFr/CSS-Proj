d2f56f6d5241edba4154745f3a4efeb580da4d25
xas5382 <156216397+xas5382@users.noreply.github.com>
2024-01-13 16:21:58 +0800

    Update Navbar.jsx

--- app/ui/Navbar.jsx
+++ app/ui/Navbar.jsx
@@ -1,1 +1,17 @@
+"use client";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
+export default function NavigationBar() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark" style={{ marginBottom: "15px" }}>
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/bus">Bus Arrival</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
