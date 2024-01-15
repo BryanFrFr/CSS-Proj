dd031b7ac42772c0bf55850c2459280ef2657064
xas5382 <swka132006@gmail.com>
2024-01-15 14:14:59 +0800

    made changes

--- app/bus/page.jsx
+++ app/bus/page.jsx
@@ -5,7 +5,6 @@ import useSWR from "swr";
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import BusData from './api/route.jsx';
 import styles from "./page.module.css";
 
 function FormTextExample() {
@@ -16,42 +15,21 @@ function FormTextExample() {
   }
   return (
     <>
-      <Container>
-        <Form.Control
-          className={styles.input}
-          type="text"
-          placeholder="Enter Bus Stop Code"
-          value={busStopCode}
-          onChange={handleInputChange} />
-      </Container>
-      <Container>
-        <Button className={styles.button} variant="outline-secondary">Get Bus Timings</Button>{' '}
-      </Container>
+      <Form.Control
+        className={styles.input}
+        type="text"
+        placeholder="Enter Bus Stop Code"
+        value={busStopCode}
+        onChange={handleInputChange} />
+      <Button className={styles.button} variant="outline-secondary">Get Bus Timings</Button>{' '}
     </>
   );
 }
 
-export default FormTextExample;
+FormTextExample;
 
-function App() {
-  const { data, error, isLoading } = useSWR(BusData);
+export default function App() {
+  const { data, error, isLoading } = useSWR("/bus/api");
+  console.log(data);
 
-  if (error) {
-    return <h1>Error loading bus arrival data: {error.message}</h1>;
-  }
-  if (isLoading) {
-    return (
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    );
-  }
-
-  // render data
-  return (
-    <section>
-      <h1>Bus Arrival Timings</h1>
-      <h1>{data.BusStopCode}</h1>
-    </section>
-  );
 }
