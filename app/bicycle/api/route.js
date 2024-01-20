e3ddada82e762dbd9435c15661ed575e2777540b
BryanFrFr <gohb3007@gmail.com>
2024-01-20 20:19:03 +0800

    idk its just telling me to

--- app/bicycle/api/route.js
+++ app/bicycle/api/route.js
@@ -1,11 +1,11 @@
   export async function GET() {
-    const res = await fetch("http://datamall2.mytransport.sg/ltaodataservice/BicycleParkingv2?Lat=1.364897&Long=103.766094", {
+    const response = await fetch("http://datamall2.mytransport.sg/ltaodataservice/BicycleParkingv2?Lat=1.364897&Long=103.766094", {
       headers: {
         'Content-Type': 'application/json',
         'AccountKey': 'gnT3uIc4Q4G151/hKaMiAg==',
       },
     });
   
-    const data = await res.json();
-    return data; // Return the data directly
+    const data = await response.json();
+    return data;
   }
