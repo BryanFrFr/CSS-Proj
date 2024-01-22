bf854dbd5a521567a9eb2236bd9161f618baa473
BryanFrFr <gohb3007@gmail.com>
2024-01-22 09:02:28 +0800

    Fixed the data returning nothing (Thanks Audrey)

--- app/bicycle/page.jsx
+++ app/bicycle/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 import useSWR from 'swr';
 import React from 'react';
-const fetcher = (...args) =>{ fetch(...args).then ((res) => res.json())}
+const fetcher = (...args) => fetch(...args).then ((res) => res.json());
 
 export default function App(){
   const {data, isLoading, error} =  useSWR('/bicycle/api', fetcher); 
-  console.log(data);
+  console.log(data.RackCount);
   console.log("hi")
   if (error) {
     return <div>failed to load</div>
