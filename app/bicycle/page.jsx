8f2e286a7ebe340bb2537ce257f8c24fc5e1efd2
BryanFrFr <gohb3007@gmail.com>
2024-01-23 21:16:24 +0800

    Implemented lat and long

--- app/bicycle/page.jsx
+++ app/bicycle/page.jsx
@@ -2,7 +2,29 @@
 import useSWR from 'swr';
 import React from 'react';
 const fetcher = (...args) => fetch(...args).then ((res) => res.json());
-
+export function latLong(){
+  if ("geolocation" in navigator) {
+    // Prompt user for location permissions
+    navigator.geolocation.watchPosition(
+      // Use watchPosition instead of getCurrentPosition as it follows user's position as they move
+      function(position) {
+        // Get the user's latitude and longitude
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        return (lat, long);
+        // Returns user position
+      },
+      // Function for errors
+      function(error) {
+        // Handle errors, e.g. user denied location sharing permissions
+        console.error("Error getting user location:", error);
+      }
+    );
+  } else {
+    // Executes if Geolocation is not supported by the browser
+    console.error("Geolocation is not supported by this browser.");
+  }
+}
 export default function App(){
   const {data, isLoading, error} =  useSWR('/bicycle/parkingApi', fetcher);
   
