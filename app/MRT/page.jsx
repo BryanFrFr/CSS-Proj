4e4ca2a4c448a7e3a12561a345520adeeafd5872
AydenSee <aydensee@gmail.com>
2024-02-05 14:36:31 +0800

    1436 5/2

--- app/MRT/page.jsx
+++ app/MRT/page.jsx
@@ -5,6 +5,11 @@ const fetcher = (...args) => fetch(...args).then ((res) => res.json());
 
 
 export default function App(){
+  var tableStyle = {
+    "border": "1px solid black",
+    "textAlign" : "center",
+    "padding": "10px",
+ };
   const {data, isLoading, error} =  useSWR('/MRT/api', fetcher); 
   if (error) {
     return <h1>Failed to load</h1>
@@ -13,6 +18,21 @@ export default function App(){
     return <h1>Loading...</h1>
   }
   return(
-    <div>{data.value[0].Station}</div>
+    <div>
+    <table style = {tableStyle}>
+        <tr><th style={tableStyle}>Station</th><th style={tableStyle}>Start Time</th><th style={tableStyle}>End Time</th><th >Crowd level</th></tr>
+      {data.value.map((val, key) => {
+                    return (
+                        <tr style={tableStyle} key={key}>
+                            <td style={tableStyle}>{val.Station}</td>
+                            <td style={tableStyle}>{val.StartTime}</td>
+                            <td style={tableStyle}>{val.EndTime}</td>
+                            <td style={tableStyle}>{val.CrowdLevel}</td>
+                        </tr>
+                    )
+                })}
+      </table>
+      </div>
   );
 }
+
