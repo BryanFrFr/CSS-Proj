c0f440ad7fb3883765c044b27949a1384d8bcd85
xas5382 <swka132006@gmail.com>
2024-02-08 14:35:51 +0800

    removed redundant

--- app/MRT/page.jsx
+++ app/MRT/page.jsx
@@ -2,7 +2,7 @@
 
 "use client";
 import useSWR from 'swr';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
